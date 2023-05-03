c6f49777342c9b1622f5b65a4bdee050eba24b5c
anhthu1402 <20520792@gm.uit.edu,vn>
2023-05-03 23:39:47 +0700

    modify page About - not completed

---
+++ src/pages/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import "./About.css";
+import AppAppBar from "../../components/About/AppBar/AppAppBar";
+import { Container } from "@mui/material";
+import ProductCategories from "../../components/About/ProductCategories/ProductCategories";
+
+function About() {
+  return (
+    <Container>
+      <AppAppBar />
+      <ProductCategories />
+      <Footer />
+    </Container>
+  );
+}
+
+export default About;
